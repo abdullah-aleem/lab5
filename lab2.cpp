08a8400bdc30827ed2f2e4eb8eb0bcd6feb40362
abdullah-aleem <abdullahaleem2102@gmail.com>
2022-10-23 13:52:16 +0500

    secondCommit

--- lab2.cpp
+++ lab2.cpp
@@ -10,7 +10,7 @@ bool moveMin(vector<int> &in, vector<int> &out){
   int flag = true;
   int temp;
 
-
+  
     for (int j = in.size()-1; j >0; j--) {
      
       if (in[j] < in[j - 1]){
@@ -21,8 +21,12 @@ bool moveMin(vector<int> &in, vector<int> &out){
         
       }
       else
-        {break;}
+      {
+
+        break;
+      }    
     };
+  
     return true;
 }
 bool testMoveMin(){
@@ -32,7 +36,7 @@ bool testMoveMin(){
 
   srand(time(0));
   dice = rand() % 100 + 1;
-   for (int i = 0; i < 5; i++) {
+   for (int i = 0; i < 100000; i++) {
 
     temp.push_back(rand()%100+1);
 
@@ -40,14 +44,33 @@ bool testMoveMin(){
   }
 
   sort(temp.begin(), temp.end());
-  temp.push_back(rand() % 100 + 1);
+  temp.push_back(101);
   temp2 = temp;
  
 
    sort(temp2.begin(),temp2.end());
-   
+   struct timeval start, end;
+
+   // start timer.
+   gettimeofday(&start, NULL);
+
+   // unsync the I/O of C and C++.
+   ios_base::sync_with_stdio(false);
    moveMin(temp, temp2);
 
+   gettimeofday(&end, NULL);
+
+   // Calculating total time taken by the program.
+   double time_taken;
+
+   time_taken = (end.tv_sec - start.tv_sec) * 1e6;
+   time_taken = (time_taken + (end.tv_usec -
+                               start.tv_usec)) *
+                1e-6;
+
+   cout << "Time taken by program is : " << fixed
+        << time_taken;
+   cout << " sec" << endl;
    if (temp == temp2) {
      return true;
   }
@@ -55,28 +78,9 @@ bool testMoveMin(){
 }
 
 int main() {
-  struct timeval start, end;
-
-  // start timer.
-  gettimeofday(&start, NULL);
-
-  // unsync the I/O of C and C++.
-  ios_base::sync_with_stdio(false);
+  
 
   cout << testMoveMin()<<endl;
 
-  gettimeofday(&end, NULL);
-
-  // Calculating total time taken by the program.
-  double time_taken;
-
-  time_taken = (end.tv_sec - start.tv_sec) * 1e6;
-  time_taken = (time_taken + (end.tv_usec -
-                              start.tv_usec)) *
-               1e-6;
-
-  cout << "Time taken by program is : " << fixed
-       << time_taken ;
-  cout << " sec" << endl;
   return 0;
   }
